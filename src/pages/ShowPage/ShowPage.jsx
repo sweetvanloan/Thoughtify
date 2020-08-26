557b2120cf3c42548a621c929d8bc333c79129f7
Ian Chu <66397920+forkitupforkitup@users.noreply.github.com>
2020-08-26 16:43:52 -0700

    Some routes and expanded CRUD

---
+++ src/pages/ShowPage/ShowPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+function ShowPage(props) {
+    return (
+        <div>
+            <h1>title here</h1>
+            <p>body here</p>
+        </div>
+    )
+}
+export default ShowPage;
